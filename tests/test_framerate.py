f4692d568c8c60b36c2f9ed569cb863aa73adc87
AdricEpic <adric.worley@epicgames.com>
2015-12-17 19:26:01 -0500

    Adds Framerate class and tests. There's a lot of book-keeping related to the concept of framerate, so it seems reasonable to move all of that logic into its own class.

---
+++ tests/test_framerate.py
@@ -0,0 +1,182 @@
+import unittest
+import sys
+
+from timecode.framerate import Framerate, FramerateError
+
+
+class FramerateTests(unittest.TestCase):
+    # Timecodes
+    NTSC_NDF_rates = ['24', '30', '60']
+    NTSC_DF_rates = ['23.98', '29.97', '59.94']
+    PAL_rates = ['25', '50']
+    misc_rates = {"ms": 1000, "frames": 1}
+
+    DF_to_NDF = dict(zip(NTSC_DF_rates, NTSC_NDF_rates))
+    NDF_to_DF = dict(zip(NTSC_NDF_rates, NTSC_DF_rates))
+
+    # Valid framerate input values
+    valid_int_values = [24, 25, 30, 50, 60]
+    valid_float_values = [23.98, 24.0, 25.00, 29.97, 30.000, 50.0, 59.94, 60.00000000]
+    valid_str_values = ['23.98', '23.980', '24', '24.0', '25', '25.00', '29.97', '30', '30.0', '50',
+                        '50.00', '59.94', '59.94000', '60', '60.000000']
+    valid_unicode_values = [u'23.98', u'23.980', u'24', u'24.0', u'25', u'25.00', u'29.97', u'30', u'30.0', u'50',
+                            u'50.00', u'59.94', u'59.94000', u'60', u'60.000000']
+
+    valid_value_lists = [valid_int_values, valid_float_values, valid_str_values, valid_unicode_values]
+
+    # Invalid framerate input values
+    invalid_int_values = [-sys.maxint - 1, 100, -1, 0, 1, 23, 26, 29, 31, 49, 51, 59, 61, sys.maxint]
+    invalid_float_values = [-100000000.00, -123463.34532, -.0000001, .00004, 23.979999999, 23.99999999, 59.9399999999,
+                            1000.2341234, 12349082341234]
+    invalid_str_values = ['not_a_number', '-205.0', '24.0.1', '23.9800000001', '239485029345680']
+    invalid_unicode_values = [u'not_a_number', u'-205.0', u'24.0.1', u'23.9800000001', u'239485029345680']
+    invalid_type_values = [None, ['24'], (29.97, 30), {'fps': 30}]
+
+    invalid_value_lists = [invalid_int_values, invalid_float_values, invalid_str_values, invalid_unicode_values,
+                           invalid_type_values]
+
+    def test_init_valid_framerates(self):
+        """Test initialization with valid framerates"""
+        def verify_valid_framerate(fps):
+            fr = Framerate(fps)
+
+            # Check that Framerate value matches parameter
+            if isinstance(fps, basestring) and fps not in self.misc_rates:
+                # No try/except here; we expect this value to be valid.
+                fps = float(fps)
+            if isinstance(fps, float):
+                self.assertEqual(fr.framerate, str(int(fps)) if fps.is_integer() else str(fr))
+            else:
+                self.assertEqual(fr.framerate, str(fps))
+
+        for valid_value_list in self.valid_value_lists:
+            for valid_value in valid_value_list:
+                verify_valid_framerate(valid_value)
+
+        for valid_misc in self.misc_rates:
+            verify_valid_framerate(valid_misc)
+
+    def test_init_invalid_framerates(self):
+        """Test initialization with invalid framerates"""
+
+        def verify_invalid_framerate(fps_):
+            if isinstance(fps_, (basestring, int, float)):
+                expected_exc = ValueError
+            else:
+                expected_exc = TypeError
+            self.assertRaises(expected_exc, Framerate, fps_)
+
+        for invalid_value_list in self.invalid_value_lists:
+            for invalid_value in invalid_value_list:
+                verify_invalid_framerate(invalid_value)
+
+    def test_float_conversion(self):
+        """Test __float__ method"""
+        for number_rate in self.NTSC_NDF_rates + self.NTSC_DF_rates + self.PAL_rates:
+            fr = Framerate(number_rate)
+            self.assertEqual(float(fr), float(number_rate))
+
+        for misc_rate in self.misc_rates:
+            fr = Framerate(misc_rate)
+            self.assertEqual(float(fr), float(self.misc_rates[misc_rate]))
+
+    def test_int_conversion(self):
+        """Test __int__ method"""
+        for number_rate in self.NTSC_NDF_rates + self.NTSC_DF_rates + self.PAL_rates:
+            fr = Framerate(number_rate)
+            if number_rate in self.NTSC_DF_rates:
+                self.assertEqual(int(fr), int(round(float(fr))))
+            else:
+                self.assertEqual(int(fr), int(number_rate))
+
+        for misc_rate in self.misc_rates:
+            fr = Framerate(misc_rate)
+            self.assertEqual(int(fr), int(self.misc_rates[misc_rate]))
+
+    def test_set_valid_framerate_value(self):
+        """Test setting framerate property to valid value"""
+
+        for rate in self.NTSC_NDF_rates + self.NTSC_DF_rates + self.PAL_rates + list(self.misc_rates.iterkeys()):
+            fr = Framerate(24)
+            fr.framerate = rate
+            self.assertEqual(rate, fr.framerate)
+
+    def test_set_invalid_framerate_value(self):
+        """Test setting framerate property to invalid value"""
+        def set_value(obj, value):
+            assert(isinstance(obj, Framerate))
+            obj.framerate = value
+
+        def verify_invalid_framerate(rate):
+            if isinstance(rate, (basestring, int, float)):
+                expected_exc = ValueError
+            else:
+                expected_exc = TypeError
+            fr = Framerate(24)
+            self.assertRaises(expected_exc, set_value, fr, rate)
+
+        for invalid_value_list in self.invalid_value_lists:
+            for invalid_value in invalid_value_list:
+                verify_invalid_framerate(invalid_value)
+
+    def test_equality_against_equivalent(self):
+        """Test comparing Framerate against equivalent values"""
+        for valid_value_list in self.valid_value_lists:
+            for valid_value in valid_value_list:
+                fr = Framerate(valid_value)
+                self.assertEqual(fr, valid_value)
+
+        for valid_value in self.misc_rates.iterkeys():
+            fr = Framerate(valid_value)
+            fr2 = Framerate(valid_value)
+
+            self.assertEqual(fr, valid_value)
+            self.assertEqual(fr, fr2)
+
+    def test_equality_against_inqeual(self):
+        """Test comparing Framerate against inequal values"""
+        fr = Framerate(24)
+
+        # Compare against other valid Framerates
+        for value in self.valid_float_values:
+            if value != 24:
+                fr2 = Framerate(value)
+                self.assertNotEqual(fr, fr2)
+
+        # Compare against other non-Framerate values, both valid and invalid
+        other_values = [25, 30, 50, 60, 23.98, 25.00, 29.97, 30.000, 50.0, 59.94, 60.00000000, '23.98', '23.980', '25',
+                        '25.00', '29.97', '30', '30.0', '50', '50.00', '59.94', '59.94000', '60', '60.000000',
+                        -sys.maxint - 1, 100, -1, 0, 1, 23, 26, 29, 31, 49, 51, 59, 61, sys.maxint, 'not_a_number',
+                        '-205.0', '24.0.1', '23.9800000001', '239485029345680', None, ['24'], (29.97, 30), {'fps': 30}]
+
+        for value in other_values:
+            self.assertNotEqual(fr, value)
+
+    def test_dropframe_identification(self):
+        """Test identification of dropframe rates"""
+        for rate in self.NTSC_DF_rates:
+            fr = Framerate(rate)
+            self.assertTrue(fr.isDropFrame)
+
+        for rate in self.NTSC_NDF_rates + self.PAL_rates + list(self.misc_rates.iterkeys()):
+            fr = Framerate(rate)
+            self.assertFalse(fr.isDropFrame)
+
+    def test_dropframe_rate_NTSC(self):
+        """Test NTSC rates have dropframe-equivalent rates"""
+        for rate in self.NTSC_DF_rates + self.NTSC_NDF_rates:
+            fr = Framerate(rate)
+            if rate in self.NTSC_DF_rates:
+                self.assertEqual(fr.dropFrameRate, rate)
+            else:
+                self.assertEqual(fr.dropFrameRate, self.NDF_to_DF[rate])
+
+    def test_dropframe_rate_others(self):
+        """Test non-NTSC rates raise error when getting dropframe rates"""
+        def get_property(obj):
+            assert(isinstance(obj, Framerate))
+            return obj.dropFrameRate
+
+        for rate in self.PAL_rates + list(self.misc_rates.iterkeys()):
+            fr = Framerate(rate)
+            self.assertRaises(FramerateError, get_property, fr)
---
+++ timecode/framerate.py
@@ -0,0 +1,118 @@
+class Framerate(object):
+
+    _NTSC_NDF_rates = ['24', '30', '60']
+    _NTSC_DF_rates = ['23.98', '29.97', '59.94']
+    _NTSC_rates = _NTSC_DF_rates + _NTSC_NDF_rates
+    _PAL_rates = ['25', '50']
+    # Special case framerates
+    _misc_rates = {'ms': '1000', 'frames': '1'}
+    _supported_framerates = _NTSC_NDF_rates + _NTSC_DF_rates + _PAL_rates + list(_misc_rates.iterkeys())
+
+    _DF_to_NDF = dict(zip(_NTSC_DF_rates, _NTSC_NDF_rates))
+    _NDF_to_DF = dict(zip(_NTSC_NDF_rates, _NTSC_DF_rates))
+
+    def __init__(self, frames_per_second):
+
+        self._framerate = self._validate_framerate(frames_per_second)
+
+        self._drop_frame = None
+        self._int_framerate = None
+
+    def __float__(self):
+        if self.framerate in self._misc_rates:
+            return float(self._misc_rates[self.framerate])
+        else:
+            return float(self._framerate)
+
+    def __int__(self):
+        return int(round(float(self.nonDropFrameRate)))
+
+    def __str__(self):
+        return self.framerate
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return int(self) == other
+        elif isinstance(other, float):
+            return float(self) == other
+        elif isinstance(other, basestring):
+            # Try to convert str to float to eliminate false negatives due to
+            # significant digits. If that fails, compare strings directly in
+            # case it's a misc case.
+            try:
+                return float(self) == float(other)
+            except ValueError:
+                return self.framerate == other
+        elif isinstance(other, self.__class__):
+            return self.framerate == other.framerate
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def _validate_framerate(self, fps):
+        if not isinstance(fps, (basestring, int, float)):
+            raise TypeError("Type {} not supported for fps".format(fps.__class__.__name__))
+
+        # Convert to float to make sure float strings will be accepted
+        if isinstance(fps, basestring) and fps not in self._misc_rates:
+            try:
+                fps = float(fps)
+            except ValueError:
+                raise ValueError("{} is not a valid framerate".format(fps))
+
+        # Convert number types to strings for comparison. Reduces accidentally
+        # including invalid rates by floating-point rounding. Still some margin
+        # of error, but so small it should be safely ignorable.
+        if isinstance(fps, float):
+            fps = str(int(fps)) if fps.is_integer() else str(fps)
+        elif isinstance(fps, int):
+            fps = str(fps)
+
+        if fps not in self._supported_framerates:
+            raise ValueError("Framerate {} is not supported.".format(fps))
+
+        return fps
+
+    @property
+    def framerate(self):
+        """Return current framerate value as string"""
+        return self._framerate
+
+    @framerate.setter
+    def framerate(self, value):
+        self._framerate = self._validate_framerate(value)
+
+    @property
+    def isDropFrame(self):
+        """Return True if Framerate is drop-frame"""
+        return self.framerate in self._NTSC_DF_rates
+
+    @property
+    def dropFrameRate(self):
+        """Return drop-frame equivalent of current framerate.
+        Raises ValueError if current framerate does not support drop-frame
+        """
+        # ToDo: Maybe should return None instead of raising?
+        if self._framerate not in self._NTSC_rates:
+            raise FramerateError("Framerate {} is not an NTSC rate".format(self._framerate))
+
+        return self._framerate if self._framerate in self._NTSC_DF_rates else self._NDF_to_DF[self._framerate]
+
+    @property
+    def nonDropFrameRate(self):
+        """Return non-drop-frame equivalent of current framerate.
+        Returns current framerate if already non-drop-frame or if framerate is
+        PAL.
+        """
+        if self._framerate in self._PAL_rates:
+            return self.framerate
+        elif self._framerate not in self._misc_rates:
+            return self._framerate if self._framerate in self._NTSC_NDF_rates else self._DF_to_NDF[self._framerate]
+        else:
+            return self._misc_rates[self._framerate]
+
+
+class FramerateError(Exception):
+    """Raised when an error occurs with Framerates"""
